2211a4508eb49d860b1d27cb232a45195bd79d3d
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-02-09 23:12:31 +0100

    JsLesson8.2

--- src/JSlesson/JSL8/JsLesson8.test.ts
+++ src/JSlesson/JSL8/JsLesson8.test.ts
@@ -1,4 +1,4 @@
-import {getTriangleType, Sum} from "./JsLesson8";
+import {getSum, getTriangleType, Sum} from "./JsLesson8";
 
 test("return SUM numbers", () => {
 
@@ -10,9 +10,20 @@ test("return SUM numbers", () => {
 
 test("return TriangleType", () => {
 
-    expect(getTriangleType(1,1,1)).toBe('10');
+    expect(getTriangleType(2,2,2)).toBe('10');
     expect(getTriangleType(5,5,3)).toBe('01');
     expect(getTriangleType(55,25,35)).toBe('11');
     expect(getTriangleType(2, 10, 2)).toBe('00');
+
+})
+
+test("return get sum", () => {
+
+    expect(getSum(111)).toBe(3);
+    expect(getSum(1000)).toBe(1);
+    expect(getSum(9999)).toBe(36);
+    expect(getSum(0)).toBe(0);
+    expect(getSum(101010)).toBe(3);
+
 
 })
--- src/JSlesson/JSL8/JsLesson8.ts
+++ src/JSlesson/JSL8/JsLesson8.ts
@@ -3,21 +3,24 @@ export function Sum(...num: Array<number>) {
 
 }
 
-export const getTriangleType = (a: number, b: number, c: number):string => {
+export const getTriangleType = (a: number, b: number, c: number): string => {
     if (a + b >= c && b + c >= a && c + a >= b) {
         if (a === b && b === c) {
             return "10"
         } else if (a === b || a === c || b === c) {
             return "01"
-        }
-        else {
+        } else {
             return "11"
         }
 
     } else {
         return "00"
     }
-
 }
 
+export function getSum(num: number) {
 
+    return num.toString().split("").map(s => +s).reduce((acc, el) => acc + el)
+
+
+}
