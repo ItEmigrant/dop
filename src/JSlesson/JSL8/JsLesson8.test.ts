1ee89a15fd75bbc1d1e90f27d316802254752c38
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-02-09 17:42:25 +0100

    JsLesson8.1

--- src/JSlesson/JSL8/JsLesson8.test.ts
+++ src/JSlesson/JSL8/JsLesson8.test.ts
@@ -1,9 +1,18 @@
-import {Sum} from "./JsLesson8";
+import {getTriangleType, Sum} from "./JsLesson8";
 
 test("return SUM numbers", () => {
 
     const testSum = Sum(2,2,2,2);
 
     expect(testSum).toBe(8);
+
+})
+
+test("return TriangleType", () => {
+
+    expect(getTriangleType(1,1,1)).toBe('10');
+    expect(getTriangleType(5,5,3)).toBe('01');
+    expect(getTriangleType(55,25,35)).toBe('11');
+    expect(getTriangleType(2, 10, 2)).toBe('00');
 
 })
--- src/JSlesson/JSL8/JsLesson8.ts
+++ src/JSlesson/JSL8/JsLesson8.ts
@@ -1,7 +1,23 @@
+export function Sum(...num: Array<number>) {
+    return num.reduce((acc, el) => acc + el)
 
-   export function Sum (...num:Array<number>) {
-        return num.reduce((acc,el)=>acc+el)
+}
 
+export const getTriangleType = (a: number, b: number, c: number):string => {
+    if (a + b >= c && b + c >= a && c + a >= b) {
+        if (a === b && b === c) {
+            return "10"
+        } else if (a === b || a === c || b === c) {
+            return "01"
+        }
+        else {
+            return "11"
+        }
+
+    } else {
+        return "00"
     }
+
+}
 
 
